a18b1e94bd59cae25f625c27974b7da787db7148
Torsten Mandry <torsten.mandry@opitz-consulting.com>
2013-07-17 21:34:06 +0200

    step 2.1 - introduce function bigX(xsRGB)

--- src/main/groovy/de/opitzconsulting/katas/relativeluminance/Color.groovy
+++ src/main/groovy/de/opitzconsulting/katas/relativeluminance/Color.groovy
@@ -15,4 +15,10 @@ class Color {
     BigDecimal relativeLuminance() {
         return 0.2126 * bigR + 0.7152 * bigG + 0.0722 * bigB
     }
+
+    BigDecimal bigX(BigDecimal xsRGB) {
+        if (xsRGB <= 0.03928)
+            return xsRGB / 12.92
+        return Math.pow((xsRGB + 0.055) / 1.055, 2.4)
+    }
 }
--- src/test/groovy/de/opitzconsulting/katas/relativeluminance/ColorSpec.groovy
+++ src/test/groovy/de/opitzconsulting/katas/relativeluminance/ColorSpec.groovy
@@ -6,7 +6,7 @@ import spock.lang.Unroll
 class ColorSpec extends Specification {
 
     @Unroll("relative luminance for R: #bigR, G: #bigG, B: #bigB is #value")
-    void "relativeLuminance"() {
+    void relativeLuminance() {
         given:
         Color color = new Color(bigR, bigG, bigB)
 
@@ -20,5 +20,21 @@ class ColorSpec extends Specification {
         0.00304 | 0.00304 | 0.00304 | 0.00304
         0.00335 | 0.00335 | 0.00335 | 0.00335
         0.00304 | 0.00335 | 0.00304 | 0.00326
+    }
+
+    @Unroll("bigX(#xsRGB) is #bigX")
+    void bigX() {
+        given:
+        Color color = new Color(0, 0, 0);
+
+        expect:
+        color.bigX(xsRGB).setScale(5, BigDecimal.ROUND_HALF_UP) == bigX
+
+        where:
+        xsRGB   | bigX
+        0       | 0
+        0.03922 | 0.00304
+        0.04314 | 0.00335
+        1       | 1
     }
 }
